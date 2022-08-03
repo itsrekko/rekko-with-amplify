b8e508257daa15c7c10248c12a94d22c2cc9f677
Mohaimen Khan <mohaimenhasan@gmail.com>
2022-08-03 16:57:21 -0700

    removed google sign up for now

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -10,8 +10,6 @@ export type AmplifyDependentResourcesAttributes = {
         "rekkoamplify": {
             "IdentityPoolId": "string",
             "IdentityPoolName": "string",
-            "HostedUIDomain": "string",
-            "OAuthMetadata": "string",
             "UserPoolId": "string",
             "UserPoolArn": "string",
             "UserPoolName": "string",
